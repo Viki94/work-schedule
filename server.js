8753b3f2c1fec5f0568e2623f4b09273968258e0
Viktoria Nedyalkova <viktoria.nedyalkova94@gmail.com>
2020-06-15 17:23:34 +0300

    Add default group for new users

--- server.js
+++ server.js
@@ -146,6 +146,7 @@ app.post("/register", function (req, res) {
     username: req.body.username,
     email: req.body.email,
     userType: req.body.userType,
+    groups: ["10"],
     picture: "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
   }),
 
